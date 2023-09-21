cb00c99b766305cb8521b9735dd1b93ccc58bd42
Tech5 <Tech5@office.local>
2023-09-21 12:55:11 +0300

    change iban

--- src/components/P2P.js
+++ src/components/P2P.js
@@ -6,7 +6,10 @@ import Swal from 'sweetalert2';
 
 export default function P2P() {
   const [url, setUrl] = useState('');
-  const [seconds, setSeconds] = useState(10);
+  const [recipient, setRecipient] = useState('');
+  const [bank, setBank] = useState('');
+  const [bic, setBic] = useState('');
+  const [seconds, setSeconds] = useState(180);
 
   useEffect(() => {
     let countdownWorker;
@@ -17,7 +20,7 @@ export default function P2P() {
 
         self.onmessage = function(event) {
           if (event.data === 'startCountdown') {
-            let seconds = 10;
+            let seconds = 180;
 
             timer = setInterval(() => {
               if (seconds === 0) {
@@ -36,7 +39,7 @@ export default function P2P() {
       const blob = new Blob([workerCode], { type: 'application/javascript' });
       countdownWorker = new Worker(URL.createObjectURL(blob));
 
-      countdownWorker.onmessage = function(event) {
+      countdownWorker.onmessage = function (event) {
         if (event.data === 'countdownFinished') {
 
         } else {
@@ -52,17 +55,20 @@ export default function P2P() {
     };
   }, []);
   useEffect(() => {
-    if (seconds === 10) {
+    if (seconds === 180) {
       axios
         .get('http://localhost:5000/p2p')
         .then((response) => {
           setUrl(response.data.IBAN);
+          setRecipient(response.data.Recipient);
+          setBank(response.data.Bank);
+          setBic(response.data.BIC);
         })
         .catch((error) => {
           console.error('Ошибка:', error);
         });
     }
-    if(seconds === 0){
+    if (seconds === 0) {
       Swal.fire({
         icon: 'error',
         title: 'Oops...',
@@ -86,23 +92,55 @@ export default function P2P() {
       <h2>Совершите перевод</h2>
       <div className={styles.card}>
         <div className={styles.cardHeader}>
-          <div>SPB</div>
+          <div>Global Payments</div>
           <div>
             <span>{('0' + Math.floor((seconds / 60) % 60)).slice(-2)}:</span>
             <span>{('0' + (seconds % 60)).slice(-2)}</span>
           </div>
         </div>
         <div className={styles.cardBody}>
-          {url ? (
-            <h3 style={{ width: '200px', wordBreak: 'break-all' }}>{url}</h3>
-          ) : (
-            <h3 style={{ width: '210px', wordBreak: 'break-all' }}>Нет свободных IBAN</h3>
-          )}
-
+          <div className={styles.creditsBody}>
+            <div className={styles.credits}>
+              <span>Iban</span>
+              {url ? (
+                <h3 style={{ width: '200px', wordBreak: 'break-all' }}>{url}</h3>
+              ) : (
+                <h3 style={{ width: '210px', wordBreak: 'break-all' }}>Нет свободных IBAN</h3>
+              )}
+            </div>
+            <div className={styles.credits}>
+              <span>Получатель</span>
+              {recipient ? (
+                <h3 style={{ width: '200px', wordBreak: 'break-all' }}>{recipient}</h3>
+              ) : (
+                <h3 style={{ width: '210px', wordBreak: 'break-all' }}>''</h3>
+              )}
+            </div>
+            <div className={styles.credits}>
+              <span>Банк получателя</span>
+              {bank ? (
+                <h3 style={{ width: '200px', wordBreak: 'break-all' }}>{bank}</h3>
+              ) : (
+                <h3 style={{ width: '210px', wordBreak: 'break-all' }}>''</h3>
+              )}
+            </div>
+            <div className={styles.credits}>
+              <span>БИК банка получателя</span>
+              {bic ? (
+                <h3 style={{ width: '200px', wordBreak: 'break-all' }}>{bic}</h3>
+              ) : (
+                <h3 style={{ width: '210px', wordBreak: 'break-all' }}>''</h3>
+              )}
+            </div>
+          </div>
           <ContentCopyIcon
-            onClick={() => navigator.clipboard.writeText(url)}
-            sx={{ padding: '5px', borderRadius: '4px', cursor: 'pointer', border: 'none' }}
+            className={styles.cardButton}
+            onClick={() => navigator.clipboard.writeText(url + '\n' + recipient + '\n' + bank + '\n' + bic)}
+            sx={{ padding: '5px', width:'40px' , height:'40px', borderRadius: '4px', cursor: 'pointer', border: 'none' }}
           />
+          {/* <button style={{ padding: '15px 5px',fontFamily:"'Nunito',sans-serif" , backgroundColor:'#38b6ff', color:'white' ,  borderRadius: '4px', cursor: 'pointer', border: 'none' }}>
+                Скопировать
+          </button> */}
         </div>
       </div>
     </div>
