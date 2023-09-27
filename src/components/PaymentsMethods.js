541b20425ba7bb4bf1429074cc4fce0f526c13ec
Tech5 <Tech5@office.local>
2023-09-27 16:27:32 +0300

    fix

--- src/components/PaymentsMethods.js
+++ src/components/PaymentsMethods.js
@@ -1,6 +1,6 @@
 import styles from '../styles/PaymentsMethods.module.scss'
 import React, { useEffect } from "react";
-import {useLocation , Link} from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import Logo from '../img/GPLogo.png'
 import Method from './Method';
 import axios from 'axios';
@@ -8,53 +8,48 @@ import axios from 'axios';
 export default function PaymentsMethods() {
     function useQuery() {
         const { search } = useLocation();
-      
-        return React.useMemo(() => new URLSearchParams(search), [search]);
-      }
-      let query = useQuery();
 
-      const methods = [
+        return React.useMemo(() => new URLSearchParams(search), [search]);
+    }
+    let query = useQuery();
+
+    const methods = [
         {
-            id:1,
+            id: 1,
             name: 'PinPay',
-            link:`/pinpay?brand=${query.get('brand')}`,
-            instruction:['1. Полный Эквайринг без верефикации', "2. Заполнить реальные данные клиента ( если нет возможности то просто левые реальные данные)", "3. Создавшуюся ссылку скопировать и отправить клиенту", "4. Проверка зачисления через ПСП"],
-            brands: ['SafeInvest','VetalInvest','InfinityInvest','Revolut','RiseInvest']
+            link: `/pinpay?brand=${query.get('brand')}`,
+            instruction: ['1. Полный Эквайринг без верефикации', "2. Заполнить реальные данные клиента ( если нет возможности то просто левые реальные данные)", "3. Создавшуюся ссылку скопировать и отправить клиенту", "4. Проверка зачисления через ПСП"],
+            brands: ['SafeInvest', 'VetalInvest', 'InfinityInvest', 'Revolut', 'RiseInvest']
         },
-        // {
-        //     name: '2',
-        //     link:`https://front-pay.vercel.app/global_payments?brand=${query.get('brand')}`,
-        //     instruction:['1. Включаем ВПН страны где находится клиент', "2. Открываем ссылку, заполняем реальные данные клиента", "3. Выбираем нужный вариант оплаты из 3 више указаных", "4. Отправляем клиенту на заполнение", "5. После успешного пополнения отправляем в Тикет запрос на зачисление в формате: Название платежки 'Inserix', Почта клиента, Сумма и время пополнения, Документы Лида"],
-        // },
         {
-            id:3,
+            id: 3,
 
             name: 'P2P',
-            link:`/p2p?brand=${query.get('brand')}`,
-            instruction:['1. Включаем ВПН страны где находится клиент', "2. Открываем ссылку, заполняем реальные данные клиента", "3. Выбираем нужный вариант оплаты из 3 више указаных", "4. Отправляем клиенту на заполнение", "5. После успешного пополнения отправляем в Тикет запрос на зачисление в формате: Название платежки 'Inserix', Почта клиента, Сумма и время пополнения, Документы Лида"],
-            brands:['VetalInvest']
+            link: `/p2p?brand=${query.get('brand')}`,
+            instruction: ['1. Включаем ВПН страны где находится клиент', "2. Открываем ссылку, заполняем реальные данные клиента", "3. Выбираем нужный вариант оплаты из 3 више указаных", "4. Отправляем клиенту на заполнение", "5. После успешного пополнения отправляем в Тикет запрос на зачисление в формате: Название платежки 'Inserix', Почта клиента, Сумма и время пополнения, Документы Лида"],
+            brands: ['VetalInvest']
         },
         {
-            id:4,
+            id: 4,
 
             name: 'WLX',
-            link:`https://merchantaccount.dev/create-transaction/iycg4swp71f8hoq`,
-            instruction:['P2P на KZ, RU и Молдову'],
-            brands:['VetalInvest']
+            link: `https://merchantaccount.dev/create-transaction/iycg4swp71f8hoq`,
+            instruction: ['P2P на KZ, RU и Молдову'],
+            brands: ['VetalInvest']
         },
         {
-            id:5,
+            id: 5,
 
             name: 'Inserix',
-            link:query.get('brand') === 'SafeInvest'?'https://app.insirex.com/en/referral_form?trader%5Blabel%5D=IOVT':`https://app.insirex.com/referral_form?trader[label]=IOVC` ,
-            instruction:["1.Эквайринг с верефикацией документов после оплаты",
-            '2. Включаем ВПН страны где находится клиент', 
-            "3. Открываем ссылку, заполняем реальные данные клиента",
-            `4. Обязательно указываем этот крипто кошелёк: ${query.get('brand') === 'SafeInvest'? 'bc1q5cf5cng938avegtadkuf98hwfyvwwdd0sqgezk' : 'bc1qrsr8q37neh7cf9sqmfau3a8gu4u6qngpev23rf'} , иначе деньги не будут зачислены`,
-            "5.  Выбираем нужный вариант оплаты ( CC LPCS/WLC CC/ ZEN), не прошел один, значит пройдет другой", 
-            "6. Отправляем клиенту на заполнение данных карты и оптаты", 
-            "7. После успешного пополнения отправляем в Тикет запрос на зачисление в формате: Название платежки 'Inserix', Почта клиента, Сумма и время пополнения, Документы Лида"],
-            brands: ['SafeInvest','VetalInvest','InfinityInvest','Revolut','RiseInvest']
+            link: query.get('brand') === 'SafeInvest' ? 'https://app.insirex.com/en/referral_form?trader%5Blabel%5D=IOVT' : `https://app.insirex.com/referral_form?trader[label]=IOVC`,
+            instruction: ["1.Эквайринг с верефикацией документов после оплаты",
+                '2. Включаем ВПН страны где находится клиент',
+                "3. Открываем ссылку, заполняем реальные данные клиента",
+                `4. Обязательно указываем этот крипто кошелёк: ${query.get('brand') === 'SafeInvest' ? 'bc1q5cf5cng938avegtadkuf98hwfyvwwdd0sqgezk' : 'bc1qrsr8q37neh7cf9sqmfau3a8gu4u6qngpev23rf'} , иначе деньги не будут зачислены`,
+                "5.  Выбираем нужный вариант оплаты ( CC LPCS/WLC CC/ ZEN), не прошел один, значит пройдет другой",
+                "6. Отправляем клиенту на заполнение данных карты и оптаты",
+                "7. После успешного пополнения отправляем в Тикет запрос на зачисление в формате: Название платежки 'Inserix', Почта клиента, Сумма и время пополнения, Документы Лида"],
+            brands: ['SafeInvest', 'VetalInvest', 'InfinityInvest', 'Revolut', 'RiseInvest']
         }
     ]
     // useEffect(()=>{
@@ -71,19 +66,19 @@ export default function PaymentsMethods() {
     // },[])
     const getMethods = () => {
         const brand = query.get('brand')
-        const arr = methods.filter(el=> el.brands.includes(brand))
+        const arr = methods.filter(el => el.brands.includes(brand))
         return arr
     }
-    return(
+    return (
         <div className={styles.body}>
-            <Link to={'/'} style={{color:'white', fontWeight:'bold' , fontFamily:'"Montserrat" , sans-serif' , position:'absolute' , top:'20px' , right:'20px'}}>На главную</Link>
+            <Link to={'/'} style={{ color: 'white', fontWeight: 'bold', fontFamily: '"Montserrat" , sans-serif', position: 'absolute', top: '20px', right: '20px' }}>На главную</Link>
 
             <div className={styles.logo}>
-                <img src={Logo}/>
+                <img src={Logo} alt='logo' />
             </div>
             <h2>Пожалуйста выберите метод платежа</h2>
             <div className={styles.methods}>
-                {getMethods()?getMethods().map(el=><Method key={el.id} name={el.name} link={el.link} instruction={el.instruction}/>):''}
+                {getMethods() ? getMethods().map(el => <Method key={el.id} name={el.name} link={el.link} instruction={el.instruction} />) : ''}
             </div>
         </div>
     )
