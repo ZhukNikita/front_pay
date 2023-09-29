2b1980fbdd09d8e169dc70ea1aec064deb89bbef
Tech5 <Tech5@office.local>
2023-09-29 10:55:40 +0300

    wlx

--- src/pages/WLXTransactions/Transaction.js
+++ src/pages/WLXTransactions/Transaction.js
@@ -1,23 +1,41 @@
 import styles from './WlxTransactionsList.module.scss'
-
+import TimerOffIcon from '@mui/icons-material/TimerOff';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
+import TimerIcon from '@mui/icons-material/Timer';
 
 export default function Transaction({transaction}) {
     const date = new Date(transaction.created_at);
     const formattedDate = `${date.getDate()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getFullYear()} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
     
+
+    function getStatus(status){
+        if(status === 'pending'){
+            return (<div className={styles.pending}><TimerIcon/> {status}</div>)
+        }if(status === 'success'){
+            return (<div className={styles.success}><CheckCircleIcon/> {status}</div>)
+        }if(status === 'rejected'){
+            return (<div className={styles.rejected}><CancelIcon/> {status}</div>)
+        }if(status === 'timeout'){
+            return (<div className={styles.rejected}><TimerOffIcon/> {status}</div>)
+        }else{
+            return status
+        }
+    }
     return(
         <div className={styles.transaction}>
             <div className={styles.body}>
             <h3 style={{ width: '7vw' }}>{formattedDate}</h3>
                 <h3 style={{ width: '160px' }}>{transaction.uuid}</h3>
-                <h3 style={{ width: '160px' }}>{transaction.currency}</h3>
+                <h3 style={{ width: '7vw' }}>{transaction.currency}</h3>
                 <h3 style={{ width: '10vw' }}>{transaction.customer_uid}</h3>
-                <h3 style={{ width: '7vw' }}>{transaction.entered_amount}</h3>
-                <h3 style={{ width: '9vw' }}>{transaction.sent_amount}</h3>
+                <h3 style={{ width: '8vw' }}>{transaction.entered_amount}</h3>
+                <h3 style={{ width: '9.5vw' }}>{transaction.sent_amount}</h3>
                 <h3 style={{ width: '7vw' }}>
-                    {transaction.status}
+                    {getStatus(transaction.status)}
                 </h3>
             </div>
         </div>
     )
 }
+//pending
--- src/pages/WLXTransactions/WlxTransactionsList.js
+++ src/pages/WLXTransactions/WlxTransactionsList.js
@@ -3,9 +3,18 @@ import {useState,useEffect} from 'react'
 import axios from 'axios'
 import Transaction from './Transaction';
 import { Pagination } from '@mui/material';
+import LinearProgress from '@mui/material/LinearProgress';
 export default function WlxTransactionsList() {
     const [transactions, setTransactions] = useState([])
     const [search, setSearch] = useState('')
+    const [usersPerPage] = useState(5);
+    const [currentPage, setCurrentPage] = useState(1);
+    const totalFilteredUsers = transactions.length;
+    const totalPageCount = Math.ceil(totalFilteredUsers / usersPerPage);
+    const indexOfLastUser = currentPage * usersPerPage;
+    const indexOfFirstUser = indexOfLastUser - usersPerPage;
+    const currentUser = transactions.slice(indexOfFirstUser, indexOfLastUser);
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     useEffect(()=>{
         const fetchData = async()=>{
@@ -21,7 +30,7 @@ export default function WlxTransactionsList() {
         }   
         fetchData()
     },[])
-    console.log(transactions)
+    console.log(transactions[0])
     return(
         <div className={styles.transactionsList}>
             <div className={styles.search}>
@@ -31,6 +40,7 @@ export default function WlxTransactionsList() {
                     value={search}
                     onChange={(e) => setSearch(e.target.value)}
                 />
+                <LinearProgress sx={{width:'40%'}} color="success"/>
             </div>
             <div className={styles.header}>
                 <h3 style={{ width: '7vw' }}>Дата</h3>
@@ -42,22 +52,25 @@ export default function WlxTransactionsList() {
                 <h3 style={{ width: '7vw' }}>Статус</h3>
             </div>
             {
-                transactions.map(el=> <Transaction key={el.id} setTransactions={setTransactions} transaction={el}/>)
+                currentUser.map(el=> <Transaction key={el.id} setTransactions={setTransactions} transaction={el}/>)
             }
                   <div
                         style={{
-                        width: '100%',
+                        width: '97%',
                         display: 'flex',
-                        justifyContent: 'right',
-                        padding: '10px 0px',
+                        justifyContent: 'space-between',
+                        alignItems:'center',
+                        padding: '10px 10px 10px 10px',
+                        margin:'0 auto',
                         }}
                     >
+                        <span style={{color:'white'}}>Всего транзакций: {transactions.length}</span>
                         <Pagination
-                        // count={totalPageCount}
+                        count={totalPageCount}
                         color="primary"
                         shape="rounded"
-                        // page={currentPage}
-                        // onChange={(event, page) => paginate(page)}
+                        page={currentPage}
+                        onChange={(event, page) => paginate(page)}
                         />
                     </div>
         </div>
