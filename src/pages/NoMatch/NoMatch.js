21d9d58588f6740cc9b82466630c84afcbdf2a8d
Tech5 <Tech5@office.local>
2023-10-02 11:47:47 +0300

    fix

--- src/pages/NoMatch/NoMatch.js
+++ src/pages/NoMatch/NoMatch.js
@@ -1,12 +1,25 @@
+import secureLocalStorage from 'react-secure-storage'
 import styles from '../../styles/P2P.module.scss'
 import {Link} from 'react-router-dom'
 
 export default function NoMatch(){
+    const role = secureLocalStorage.getItem('role')
+    function getLink(){
+        if(role === null) {
+            return '/login'
+        }
+        if(role === 'User') {
+            return '/payments_methods'
+        }
+        if(role === 'Admin' || role === 'SuperAdmin' || role === 'Financier'  ) {
+            return '/panel'
+        }
+    }
     return(
         <div className={styles.body}>
             <h1 style={{color:'#fff' , fontSize:'80px'}}>404</h1>
             <h1 style={{color:'#fff'}}>Такой страницы не существует</h1>
-            <Link to={'/payments_methods'}
+            <Link to={getLink()}
             style={{marginTop:'20px',
             border:'none', 
             backgroundColor:'rgb(56, 182, 255)' , 
