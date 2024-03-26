91ee0c0b5e55085497f24c02b8649cfb43cc7365
ZhukNikita <nikitazhuk99@icloud.com>
2024-03-26 16:06:11 +0200

    fix

---
+++ src/pages/NowPayTransactions/NowPayTransactions.js
@@ -0,0 +1,24 @@
+import NavBar from '../../components/NavBar'
+import styles from './NowPayTransactions.module.scss'
+import NowPayTransactionsBody from './NowPayTransactionsBody'
+import secureLocalStorage from 'react-secure-storage'
+import { Navigate } from 'react-router-dom'
+
+
+export default function NowPayTransactions() {
+  if(!secureLocalStorage.getItem('isLogged')){
+    return <Navigate to={'/login'}/>
+  }
+  if(secureLocalStorage.getItem('role') === 'User' && secureLocalStorage.getItem('isLogged')){
+    return <Navigate to={'/payments_methods'}/>
+  }
+  if(!secureLocalStorage.getItem('methods').includes('shp.ee')){
+    return <Navigate to={'/payments_methods'}/>
+  }
+  return(
+    <div className={styles.body}>
+      <NavBar/>
+      <NowPayTransactionsBody/>
+    </div>
+  )
+}
